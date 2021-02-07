a1078c2536e00ebcdfab721306de99c67049b6c8
jdpv01 <juandpv01@hotmail.com>
2021-02-07 11:48:51 -0500

    open file displays data now

--- report/Form1.Designer.cs
+++ report/Form1.Designer.cs
@@ -30,22 +30,46 @@ namespace report
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.cbFilter = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(153, 86);
+            this.dataGridView1.Location = new System.Drawing.Point(1, 1);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(797, 252);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(723, 259);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // cbFilter
+            // 
+            this.cbFilter.FormattingEnabled = true;
+            this.cbFilter.Location = new System.Drawing.Point(596, 259);
+            this.cbFilter.Name = "cbFilter";
+            this.cbFilter.Size = new System.Drawing.Size(121, 21);
+            this.cbFilter.TabIndex = 2;
+            this.cbFilter.SelectedIndexChanged += new System.EventHandler(this.cbFilter_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cbFilter);
+            this.Controls.Add(this.btnOpen);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -57,6 +81,9 @@ namespace report
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.ComboBox cbFilter;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- report/Form1.cs
+++ report/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +18,40 @@ namespace report
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog
+            {
+                Filter = "Excel | *.xls;*.xlsx;",
+                Title = "Select File"
+            };
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                dataGridView1.DataSource = importData(ofd.FileName);
+            }
+        }
+
+        private DataView importData(string fileName)
+        {
+            String connection = string.Format("Provider = Microsoft.ACE.OLEDB.12.0; Data Source = {0}; Extended Properties = 'Excel 12.0;'", fileName);
+            OleDbConnection connector = new OleDbConnection(connection);
+            connector.Open();
+            OleDbCommand query = new OleDbCommand("Select * from [DIVIPOLA-_C_digos_municipios$]", connector);
+            OleDbDataAdapter adapter = new OleDbDataAdapter
+            {
+                SelectCommand = query
+            };
+            DataSet ds = new DataSet();
+            adapter.Fill(ds);
+            connector.Close();
+            return ds.Tables[0].DefaultView;
+        }
+
+        private void cbFilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
